973876612a2ba358ee588df886d945d852b72bfc
Kanchanok Kannee <kanchanok.kan@ku.th>
2018-04-27 16:17:33 +0700

    Edited UI Login

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -1,5 +1,16 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+	@FXML
+	private TextField id;
+	@FXML
+	private TextField password;
+	@FXML
+	private Button enterButton;
+		
 
 }
