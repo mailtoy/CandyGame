aae4323b35ca1e006733533cec1eda2e836ef075
Septima777 <noppawan.kul@ku.th>
2018-04-27 16:04:46 +0700

    switches new scene in ranking

--- src/application/RankingController.java
+++ src/application/RankingController.java
@@ -1,11 +1,27 @@
 package application;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class RankingController {
 
     @FXML
     private Button backButton;
+    
+    @FXML
+    public void handleBackPage(ActionEvent event) throws IOException{
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml")); 
+        Stage stage = new Stage();
+        stage.setScene(new Scene((Parent)loader.load()));
+        stage.show();
+    
+    }
 
 }
