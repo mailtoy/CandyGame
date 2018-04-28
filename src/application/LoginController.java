a04dbd166a5936e2922818a894004952bd5763ed
Kanchanok Kannee <kanchanok.kan@ku.th>
2018-04-28 23:01:29 +0700

    check login

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -1,7 +1,12 @@
 package application;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class LoginController {
@@ -11,6 +16,32 @@ public class LoginController {
 	private TextField password;
 	@FXML
 	private Button enterButton;
+	@FXML
+	private Label status;
+	
+	@FXML
+	public void login(ActionEvent event) {
+		LoginVerifyUser login = new LoginVerifyUser(id.getText(), password.getText());
+		try {
+			// can login (have old id and pass)
+			if (login.verifyUser() == 1) {
+				// can play
+			} 
+			// incorrect
+			if (login.verifyUser() == 2){
+				status.setText("Incorrect username and/or password");
+				// login again
+			}
+			// new user
+			if (login.verifyUser() == 0) {
+				//can play
+			}
+		} catch (IllegalArgumentException e) {
+			status.setText("ERROR : " + e.getMessage());
+		} finally {
+			
+		}
+	}
 		
 
 }
---
+++ src/application/LoginVerifyUser.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.List;
+
+public class LoginVerifyUser {
+	public String id;
+	public String password;
+	public List<String> idList;
+	public List<String> passwordList;
+	public int isUser;
+	
+	public LoginVerifyUser(String id, String password) {
+		this.id = id;
+		this.password = password;
+	}
+	
+	public int verifyUser() {
+		for (String aId : idList) {
+			for (String aPassword : passwordList) {
+				if (id.equals(aId) && password.equals(aPassword) ) {
+					return 1;
+				} if (id.equals(aId) && !password.equals(aPassword) || !id.equals(aId) && !password.equals(aPassword)) {
+					return 2;
+				}
+			}
+		}
+		return 0;
+	}
+	
+
+}
