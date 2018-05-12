d28b23c310023578dee82a254a23b8d7f0418a29
Septima777 <noppawan.kul@ku.th>
2018-05-12 18:51:54 +0700

    edit background ranking

--- src/application/RankingMain.java
+++ src/application/RankingMain.java
@@ -14,7 +14,7 @@ public class RankingMain extends Application{
 			Parent root = FXMLLoader.load(getClass().getResource("RankingUI.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			primaryStage.setScene(scene); 
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
